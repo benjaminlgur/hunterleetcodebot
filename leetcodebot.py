45ee712bedf097b6b54cedd5eaef97a31706dfb9
benjaminlgur <benjaminlgur@yahoo.com>
2020-02-02 19:54:38 -0500

    Add files via upload

--- leetcodebot.py
+++ leetcodebot.py
@@ -4,7 +4,7 @@ import asyncio
 import Classmate
 
 #TOKEN = open("leetcodebottoken.txt", "rt")
-TOKEN = "TOKEN_HERE"
+TOKEN = "TOKEN HERE"
 
 
 bot = commands.Bot(command_prefix='.')
@@ -15,8 +15,11 @@ classmates = []
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="type .info for info"))
     print("LeetCodeBot is ready.")
     print("Logged in as {0.user}".format(bot))
+    print(discord.__version__)
+    
 
 @bot.command()
 async def echo(ctx, *, message):
@@ -70,13 +73,13 @@ async def my_topics(ctx):
     await ctx.send("You do not have any LeetCode Topics set yet")
 
 @bot.command()
-async def topics_clear(ctx):
+async def clear_topics(ctx):
     for person in classmates:
         if person.id == ctx.message.author.id:
             person.topics = []
 
 @bot.command()
-async def topics_remove(ctx, *, in_text):
+async def remove_topics(ctx, *, in_text):
     topicsToRemove = in_text.split(", ")
     for person in classmates:
         if person.id == ctx.message.author.id:
@@ -84,7 +87,7 @@ async def topics_remove(ctx, *, in_text):
                 person.topics.remove(toRemove)
 
 @bot.command()
-async def topic_find(ctx, *, in_text):
+async def find_topics(ctx, *, in_text):
     matches = []
     out_string = ""
     for person in classmates:
@@ -115,6 +118,39 @@ async def shared_topics(ctx):
         out_string = out_string + match + ", "
         out_string = out_string[:-2]
     await ctx.send(out_string)
+
+@bot.command()
+async def whos_leetcoding(ctx):
+    currentlyLeetcoding = []
+    out_string = ""
+    members = ctx.message.guild.members
+    for member in members:
+        for activity in member.activities:
+            if activity.type == discord.ActivityType.custom and "leetcoding now" in activity.name and member not in currentlyLeetcoding:
+                currentlyLeetcoding.append(member.name)
+        if len(currentlyLeetcoding) <= 0:
+            await ctx.send("Noone is currently Leetcoding")
+            return
+    for person in currentlyLeetcoding:
+        out_string = out_string + person + ", "
+        out_string = out_string[:-2]
+    await ctx.send(out_string)
+
+@bot.command()
+async def info(ctx):
+    multiline = """ This is a bot for people to find others to leetcode/study with add a custom status including the words leetcoding now to show that you are available
+Commands include:
+```.whos_leetcoding: Shows all people currently working on leetcode. 
+.add_topics: Type topics you want to leetcode about if multiple use a comma and space seperator , 
+.remove_topics: Type topics you wish to remove from you list of topics
+.clear_topics: Clear your list of topics
+.my_topics: lists all of your topics
+.users_topics: lists all of a users topics either by name or by @ ping
+.shared_topics: find all users who share a topic with you.
+.find_topic: find all users with a particular topic```
+    """
+    await ctx.send(multiline)
+              
 
 
     
